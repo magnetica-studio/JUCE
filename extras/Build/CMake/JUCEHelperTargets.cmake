647eef479e46eb3a7de7ccc8d8f28a7fa4ad20e4
hotwatermorning <hotwatermorning@gmail.com>
2021-11-03 12:16:46 +0900

    Ignore compile options of juce_recommended_warning_flags target for swift sources.

--- extras/Build/CMake/JUCEHelperTargets.cmake
+++ extras/Build/CMake/JUCEHelperTargets.cmake
@@ -5,15 +5,18 @@ if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_COMPILER_FRONTEND_VARIA
     target_compile_options(juce_recommended_warning_flags INTERFACE "/W4")
 elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
     target_compile_options(juce_recommended_warning_flags INTERFACE
-        -Wall -Wshadow-all -Wshorten-64-to-32 -Wstrict-aliasing -Wuninitialized
-        -Wunused-parameter -Wconversion -Wsign-compare -Wint-conversion
-        -Wconditional-uninitialized -Woverloaded-virtual -Wreorder
-        -Wconstant-conversion -Wsign-conversion -Wunused-private-field
-        -Wbool-conversion -Wextra-semi -Wunreachable-code
-        -Wzero-as-null-pointer-constant -Wcast-align
-        -Winconsistent-missing-destructor-override -Wshift-sign-overflow
-        -Wnullable-to-nonnull-conversion -Wno-missing-field-initializers
-        -Wno-ignored-qualifiers -Wswitch-enum -Wpedantic)
+        $<$<OR:$<COMPILE_LANGUAGE:C>,$<COMPILE_LANGUAGE:CXX>,$<COMPILE_LANGUAGE:OBJCXX>>:
+            -Wall -Wshadow-all -Wshorten-64-to-32 -Wstrict-aliasing
+            -Wuninitialized -Wunused-parameter -Wconversion -Wsign-compare
+            -Wint-conversion -Wconditional-uninitialized -Wconstant-conversion
+            -Wsign-conversion -Wbool-conversion -Wextra-semi -Wunreachable-code
+            -Wcast-align -Wshift-sign-overflow -Wno-missing-field-initializers
+            -Wnullable-to-nonnull-conversion -Wno-ignored-qualifiers -Wswitch-enum
+            -Wpedantic>
+        $<$<OR:$<COMPILE_LANGUAGE:CXX>,$<COMPILE_LANGUAGE:OBJCXX>>:
+            -Wzero-as-null-pointer-constant -Wunused-private-field
+            -Woverloaded-virtual -Wreorder
+            -Winconsistent-missing-destructor-override>)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     target_compile_options(juce_recommended_warning_flags INTERFACE
         -Wall -Wextra -Wstrict-aliasing -Wuninitialized -Wunused-parameter
